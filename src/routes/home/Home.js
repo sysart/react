a25a10fa0e6df0dc217f86b2c2cca35838db27ae
Juuso <juusohaikonen@hotmail.com>
2019-09-27 13:01:42 +0300

    added fetch

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,6 +3,26 @@ import { EventSeat } from "@material-ui/icons";
 import "./Home.css";
 
 class Home extends Component {
+  componentDidMount() {
+    return fetch("https://alpaca-backend.herokuapp.com/get-stocks", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(async response => {
+        console.log("res", response);
+        const data = await response.json();
+        console.log("result is", JSON.stringify(data));
+        return data;
+      })
+      .catch(err => {
+        console.log("API call error:", err);
+        return err;
+      });
+  }
+
   render() {
     return (
       <div className="homeContainer">
